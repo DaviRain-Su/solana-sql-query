26dd95d01ef52c42e0cd5d9010bbe8211f645cbf
Davirain <davirain.yin@gmail.com>
2024-05-23 19:41:55 +0800

    refactor telemetry file

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,9 @@
 use solana_query_service::configuration::get_configuration;
 use solana_query_service::startup::run;
+use solana_query_service::telemetry::get_subscriber;
+use solana_query_service::telemetry::init_subscriber;
 use sqlx::PgPool;
 use std::net::TcpListener;
-use tracing::subscriber::set_global_default;
-use tracing::Subscriber;
-use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
-use tracing_log::LogTracer;
-use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry};
-
-/// Compose multiple layers into a `tracing`'s subscriber.
-///
-/// # Implementation Notes
-///
-/// We are using `impl Subscriber` as return type to avoid having to
-/// spell out the actual type of the returned subscriber, which is
-/// indeed quite complex.
-/// We need to explicitly call out that the returned subscriber is
-/// `Send` and `Sync` to make it possible to pass it to `init_subscriber`
-/// later on.
-pub fn get_subscriber(name: String, env_filter: String) -> impl Subscriber + Send + Sync {
-    let env_filter =
-        EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new(env_filter));
-    let formatting_layer = BunyanFormattingLayer::new(name, std::io::stdout);
-    Registry::default()
-        .with(env_filter)
-        .with(JsonStorageLayer)
-        .with(formatting_layer)
-}
-
-/// Register a subscriber as global default to process span data.
-///
-/// It should only be called once!
-pub fn init_subscriber(subscriber: impl Subscriber + Send + Sync) -> anyhow::Result<()> {
-    LogTracer::init()?;
-    Ok(set_global_default(subscriber)?)
-}
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
