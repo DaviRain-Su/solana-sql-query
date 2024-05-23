544d102df0b5295e46e2694ba596583bded8a987
Davirain <davirain.yin@gmail.com>
2024-05-23 14:42:59 +0800

    remove

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,3 @@
-pub mod cmd;
-
-use clap::Parser;
-use cmd::MointerCommand;
-use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
-
-#[tokio::main]
-async fn main() -> anyhow::Result<()> {
-    tracing_subscriber::registry()
-        .with(
-            tracing_subscriber::EnvFilter::try_from_default_env()
-                .unwrap_or_else(|_| "example_tokio_postgres=debug".into()),
-        )
-        .with(tracing_subscriber::fmt::layer())
-        .init();
-
-    let cmd = MointerCommand::parse();
-    cmd.run().await?;
-    Ok(())
+fn main() {
+    println!("Hello, world!");
 }
